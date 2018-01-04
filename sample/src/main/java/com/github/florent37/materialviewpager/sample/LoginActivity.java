96a4a3e3bb9a87abd539bb090f45dde0a270a1cf
cljh223 <cljh223@naver.com>
2018-01-04 17:01:40 +0900

    login.xml 초안 생성

---
+++ sample/src/main/java/com/github/florent37/materialviewpager/sample/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.github.florent37.materialviewpager.sample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+    }
+}
